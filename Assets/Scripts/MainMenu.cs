958b5161746b880a70e2af3b2e07ec7496387d97
aaronandyhealy <aaronhealy2010@hotmail.com>
2019-02-25 13:02:37 +0000

    Myo MainMenu Working

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,38 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
 public class MainMenu : MonoBehaviour {
 
-    public string loadLevel;
-    public string loadOne;
-    public string loadTwo;
-    public string loadThree;
+    public GameObject myo = null;
 
-    // Load Level One
-    public void PlayLevelOne () {
-        SceneManager.LoadScene(loadOne);
-	}
+    //Items to choose when poses are made.
+    public string fingersSpread;
+    public string doubleTap;
 
-    // Load Level Two
-    public void PlayLevelTwo()
+
+    // The pose from the last update. This is used to determine if the pose has changed
+    // so that actions are only performed upon making them rather than every frame during
+    // which they are active.
+    private Pose _lastPose = Pose.Unknown;
+
+    // Update is called once per frame.
+    void Update()
     {
-        SceneManager.LoadScene(loadTwo);
+        // Access the ThalmicMyo component attached to the Myo game object.
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+
+        // Check if the pose has changed since last update.
+        // The ThalmicMyo component of a Myo game object has a pose property that is set to the
+        // currently detected pose (e.g. Pose.Fist for the user making a fist). If no pose is currently
+        // detected, pose will be set to Pose.Rest. If pose detection is unavailable, e.g. because Myo
+        // is not on a user's arm, pose will be set to Pose.Unknown.
+        if (thalmicMyo.pose != _lastPose)
+        {
+            _lastPose = thalmicMyo.pose;
+
+            // Vibrate the Myo armband when a fist is made.
+            if (thalmicMyo.pose == Pose.Fist)
+            {
+                thalmicMyo.Vibrate(VibrationType.Medium);
+
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+
+                // Change material when wave in, wave out or double tap poses are made.
+            }
+            else if (thalmicMyo.pose == Pose.FingersSpread)
+            {
+                SceneManager.LoadScene(fingersSpread);
+
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+            else if (thalmicMyo.pose == Pose.Fist)
+            {
+                Application.Quit();
+
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+            else if (thalmicMyo.pose == Pose.DoubleTap)
+            {
+                SceneManager.LoadScene(doubleTap);
+
+                ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+        }
     }
 
-    //Load Level Three
-    public void PlayLevelThree()
+    // Extend the unlock if ThalmcHub's locking policy is standard, and notifies the given myo that a user action was
+    // recognized.
+    void ExtendUnlockAndNotifyUserAction(ThalmicMyo myo)
     {
-        SceneManager.LoadScene(loadThree);
-    }
+        ThalmicHub hub = ThalmicHub.instance;
 
-    //Load Instructions Page
-    public void ViewInstructions()
-    {
-        SceneManager.LoadScene(loadLevel);
-    }
+        if (hub.lockingPolicy == LockingPolicy.Standard)
+        {
+            myo.Unlock(UnlockType.Timed);
+        }
 
-    // Quit Game
-    public void QuitGame () {
-        Application.Quit();
-	}
+        myo.NotifyUserAction();
+    }
 }
